657655c2236cd6b0a2ae80535a8975f9e3bcee65
Sowrabh0-0 <sksseervi@gmail.com>
2024-03-19 18:04:11 +0530

    Assets added and Constants created
    Assets have been registered in pubspec.yaml file and added the path of assets to the constants.dart

---
+++ lib/constants/assets_constants.dart
@@ -0,0 +1,11 @@
+class AssetsConstants {
+  static const String _svgsPath = 'assets/svgs';
+  static const String activemegaphone = '$_svgsPath/Active_Megaphone.svg';
+  static const String activecalendarcheck = '$_svgsPath/Active_CalendarCheck.svg';
+  static const String activesiren = '$_svgsPath/Active_Siren.svg';
+  static const String activetram = '$_svgsPath/Active_Tram.svg';
+  static const String megaphone = '$_svgsPath/Megaphone.svg';
+  static const String calendarcheck = '$_svgsPath/CalendarCheck.svg';
+  static const String siren = '$_svgsPath/Siren.svg';
+  static const String tram = '$_svgsPath/Tram.svg';
+}
